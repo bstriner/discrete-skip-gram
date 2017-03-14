f7738bd9c1c0c3dd47a7e8777edb72fbad6516ef
bstriner <bstriner@yahoo.com>
2017-03-13 22:28:23 -0400

    reduce lr

--- examples/brown_discrete_skip_gram.py
+++ examples/brown_discrete_skip_gram.py
@@ -15,7 +15,7 @@ def main():
 
     train_discrete_skip_gram(path_generated, path_autoencoded, path_encoded, path_model, dataset, prior,
                window=7, hidden_dim=512, nb_epoch=1001,
-               nb_batch=256, batch_size=128, lr=1e-4,
+               nb_batch=256, batch_size=128, lr=3e-5,
                checkpoint_frequency=50, regularizer=None,
                encode_deterministic=False, decode_deterministic=False,
                adversarial_x=False, adversarial_z=False,
