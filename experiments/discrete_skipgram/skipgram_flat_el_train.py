250efa2533132a32d9d15d13b3f81ec68221e4b6
bstriner <bstriner@gmail.com>
2017-08-03 03:11:21 -0400

    skipgram_flat_el_train.py

--- experiments/discrete_skipgram/skipgram_flat_el_train.py
+++ experiments/discrete_skipgram/skipgram_flat_el_train.py
@@ -5,7 +5,7 @@ from discrete_skip_gram.skipgram.cooccurrence import load_cooccurrence
 from discrete_skip_gram.skipgram.regularizers import ExclusiveLasso
 from discrete_skip_gram.skipgram.util import write_csv
 from keras.optimizers import Adam
-
+from tqdm import tqdm
 
 # os.environ["THEANO_FLAGS"]="optimizer=None,device=cpu"
 
@@ -16,12 +16,12 @@ def main():
     outputpath = "output/skipgram_flat-el"
     cooccurrence = load_cooccurrence('output/cooccurrence.npy').astype(np.float32)
     data = []
-    for name, weight in [
+    for name, weight in tqdm([
         ("1e7", 1e-7),
         ("1e8", 1e-8),
         ("1e9", 1e-9),
         ("1e10", 1e-10),
-        ("1e11", 1e-11)]:
+        ("1e11", 1e-11)], desc="Meta-iteration"):
         datum = train_model(
             outputpath="{}/{}".format(outputpath, name),
             pz_weight_regularizer=ExclusiveLasso(weight),
