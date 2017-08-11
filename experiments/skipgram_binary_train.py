4f23f92cf26fec44bb9088b2b1c732639a06dcc8
bstriner <bstriner@gmail.com>
2017-08-11 18:20:53 -0400

    skipgram_binary_train.py

--- experiments/skipgram_binary_train.py
+++ experiments/skipgram_binary_train.py
@@ -1,33 +1,24 @@
 import numpy as np
-from discrete_skip_gram.skipgram.cooccurrence import load_cooccurrence
-from discrete_skip_gram.skipgram.flat_model import train_model
-from keras.optimizers import Adam
-from tqdm import tqdm
+from discrete_skip_gram.flat_train import train_flat_battery
 
-from discrete_skip_gram.util import write_csv
 
 
 # os.environ["THEANO_FLAGS"]="optimizer=None,device=cpu"
 
 def main():
     epochs = 10
-    iters = 10
+    iters = 5
     batches = 4096
     z_k = 2
     outputpath = "output/skipgram_binary"
-    cooccurrence = load_cooccurrence('output/cooccurrence.npy').astype(np.float32)
-    data = []
-    for i in tqdm(range(iters), 'Training iterations'):
-        datum = train_model(outputpath="{}/iter-{}".format(outputpath, i),
-                            epochs=epochs,
-                            batches=batches,
-                            cooccurrence=cooccurrence,
-                            z_k=z_k,
-                            opt=Adam(1e-3))
-        data.append([i] + datum)
-    header = ['Iter', 'Nll', 'Utilization']
-    write_csv("{}.csv".format(outputpath), data, header=header)
-    np.save("{}.npy".format(outputpath), np.array(data))
+    cooccurrence = np.load('output/cooccurrence.npy').astype(np.float32)
+    train_flat_battery(
+        outputpath=outputpath,
+        epochs=epochs,
+        batches=batches,
+        z_k=z_k,
+        iters=iters,
+        cooccurrence=cooccurrence)
 
 
 if __name__ == "__main__":
