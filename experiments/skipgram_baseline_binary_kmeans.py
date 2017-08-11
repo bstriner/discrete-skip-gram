6a867d1efe36f214ead4f6d4a4c1ba921f55d606
bstriner <bstriner@gmail.com>
2017-08-11 19:11:18 -0400

    increase iterations

--- experiments/skipgram_baseline_binary_kmeans.py
+++ experiments/skipgram_baseline_binary_kmeans.py
@@ -5,7 +5,7 @@ from discrete_skip_gram.clustering.kmeans import validate_cluster_km
 
 def main():
     bzks = [512, 256, 128, 64, 32]
-    iters = 5
+    iters = 20
     z_k = 2
     output_path = "output/skipgram_baseline_binary_kmeans"
     cooccurrence = np.load('output/cooccurrence.npy')
