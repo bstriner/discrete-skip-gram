9b85e88bfb468d624dc0594712aa2ba61de7a349
bstriner <bstriner@gmail.com>
2017-08-19 20:24:38 -0400

    flat bgmm

--- discrete_skip_gram/clustering/gmm.py
+++ discrete_skip_gram/clustering/gmm.py
@@ -8,36 +8,44 @@ from sklearn.mixture import GaussianMixture
 from ..flat_validation import validate_encoding_flat
 
 
-def cluster_recursive(words, code, depth):
+def cluster_hgmm_recursive(words, code, depth):
     if depth == 0:
         return [(code, word) for word in words]
     else:
         if len(words) == 1:
-            return cluster_recursive(words, code + [0], depth - 1)
+            return cluster_hgmm_recursive(words, code + [0], depth - 1)
         else:
             gmm = GaussianMixture(n_components=2, n_init=5)
             z = np.stack(w['z'] for w in words)
             gmm.fit(z)
             p = gmm.predict_proba(z)
-            score = p[:, 0] - p[:, 1]
+            eps = 1e-9
+            score = np.log(eps+p[:, 0]) - np.log(eps+p[:, 1])
             order = np.argsort(score)
             orderedwords = [words[order[j]] for j in range(order.shape[0])]
             half = int(len(words) / 2)
             a = orderedwords[:half]
             b = orderedwords[half:]
-            ar = cluster_recursive(a, code + [0], depth - 1)
-            br = cluster_recursive(b, code + [1], depth - 1)
+            ar = cluster_hgmm_recursive(a, code + [0], depth - 1)
+            br = cluster_hgmm_recursive(b, code + [1], depth - 1)
             return ar + br
 
 
-def cluster_gmm(z, depth):
+def cluster_hgmm(z, depth):
     words = [{'idx': i, 'z': z[i, :]} for i in range(z.shape[0])]
-    coded = cluster_recursive(words, [], depth)
+    coded = cluster_hgmm_recursive(words, [], depth)
     coded.sort(key=lambda _x: _x[1]['idx'])
     encodings = [np.array(_x[0]) for _x in coded]
     enc = np.stack(encodings)
     return enc
 
+
+def validate_cluster_hmm(z, z_k, cooccurrence):
+    depth = int(np.log2(z_k))
+    assert 2**depth == z_k
+    enc = cluster_hgmm(z, depth)
+    nll = validate_encoding_flat(cooccurrence=cooccurrence, enc=enc)
+    return nll
 
 def cluster_gmm_flat(z, z_k, n_init=1):
     gmm = GaussianMixture(n_components=z_k, n_init=n_init)
---
+++ experiments/skipgram_baseline_flat_bgmm.py
@@ -0,0 +1,27 @@
+import numpy as np
+from discrete_skip_gram.clustering.cluster_train import train_cluster_battery
+from discrete_skip_gram.clustering.gmm import validate_cluster_hmm
+
+
+def main():
+    bzks = [512, 256, 128, 64, 32]
+    iters = 50
+    z_k = 2
+    output_path = "output/skipgram_baseline_flat_bgmm"
+    cooccurrence = np.load('output/cooccurrence.npy')
+    input_paths = ['output/skipgram_baseline', 'output/skipgram_baseline-l1', 'output/skipgram_baseline-l2']
+    labels = ['baseline', 'l1', 'l2']
+    train_cluster_battery(
+        output_path=output_path,
+        input_paths=input_paths,
+        labels=labels,
+        bzks=bzks,
+        iters=iters,
+        z_k=z_k,
+        desc="Flat BGMM",
+        cooccurrence=cooccurrence,
+        val_fun=validate_cluster_hmm)
+
+
+if __name__ == "__main__":
+    main()
